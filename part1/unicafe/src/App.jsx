e88c9762386e51498b54dba975dd9e60a5230402
Manmohan Singh Hunjan <71703296+manmohanhunjan@users.noreply.github.com>
2023-10-28 18:44:47 +0530

    courseinfo and unicafe completed

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react'
+
+const Header = ({ text }) => <h1>{text}</h1>
+const Button = ({ text, handleClick }) => <button onClick={handleClick}>{text}</button>
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad
+  const average = (good - bad) / total
+  const positive = good / total * 100
+
+  if (total === 0) {
+    return (
+      <div>
+        No feedback given
+      </div>
+    )
+  }
+  return (
+    <table>
+      <tbody>
+        <StatisticLine text={'good'} value={good} />
+        <StatisticLine text={'neutral'} value={neutral} />
+        <StatisticLine text={'bad'} value={bad} />
+        <StatisticLine text={'all'} value={total} />
+        <StatisticLine text={'average'} value={average} />
+        <StatisticLine text={'positive'} value={positive + ' %'} />
+      </tbody>
+    </table>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <Header text={'give feedback'} />
+      <Button text={'good'} handleClick={() => setGood(good + 1)} />
+      <Button text={'neutral'} handleClick={() => setNeutral(neutral + 1)} />
+      <Button text={'bad'} handleClick={() => setBad(bad + 1)} />
+
+      <Header text={'statistics'} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+export default App
