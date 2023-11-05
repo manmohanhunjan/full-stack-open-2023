cde6e73ff599e9391af4b4b5f9bd3129cdf4c901
Manmohan Singh Hunjan <71703296+manmohanhunjan@users.noreply.github.com>
2023-11-05 06:59:52 +0530

    phonebook backend

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -66,7 +66,7 @@ const App = () => {
 
   return (
     <div>
-      <h1>Notes</h1>
+      <h1>Notes App</h1>
       <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
